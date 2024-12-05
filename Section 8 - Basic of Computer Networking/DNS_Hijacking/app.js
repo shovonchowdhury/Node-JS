a3abd40bbf30e12ea9f0953fe4d24a1742d01b29
shovonchowdhury <debojjoti550@gmail.com>
2024-12-05 17:19:27 +0600

    ports

--- Section 8 - Basic of Computer Networking/DNS_Hijacking/app.js
+++ Section 8 - Basic of Computer Networking/DNS_Hijacking/app.js
@@ -2,9 +2,9 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.end('{"message": "Hello, ProCodrrs!"}');
+  res.end('{"message": "Hello, your server has hijacked!!!"}');
 });
 
-server.listen(80, () => {
-  console.log("HTTP server is running on http://localhost");
+server.listen(4000, () => {
+  console.log("HTTP server is running on http://localhost:4000");
 });
